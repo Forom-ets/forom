99df3447d8d6fc1578159a0a38c8f1c5686fe2d1
svc-cedille-user <79548677+svc-cedille-user@users.noreply.github.com>
2026-01-30 18:12:39 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server : {
+    allowedHosts: ["forom.prodv2.cedille.club", "forom.etsmtl.ca"]
+  }
 })
