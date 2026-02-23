991b7b52aed925d203a886669eec8ef54cd1e3e0
Oxsw1ng <yannmijatovicp@gmail.com>
2026-02-22 23:55:33 -0500

    tt

--- src/components/MemoryModal.tsx
+++ src/components/MemoryModal.tsx
@@ -198,6 +198,7 @@ export function MemoryModal({
               alt={memory.title}
               className="w-full h-full object-cover"
             />
+            <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-transparent" />
           </>
         ) : (
           <div 
